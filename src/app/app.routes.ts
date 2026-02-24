d17eed7692315537e6b75ed0e5fb5f06ea55ab80
SaidiAziz <155757450+SaidiAziz@users.noreply.github.com>
2026-02-25 00:51:10 +0100

    refactor: replace placeholder content with header and footer components, add routing for landing, login, and signup pages, and implement global styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {LandingpageComponent} from './frontoffice/pages/landingpage/landingpage.component';
+import {LoginComponent} from './frontoffice/pages/login/login.component';
+import {SignupComponent} from './frontoffice/pages/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: LandingpageComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+];
