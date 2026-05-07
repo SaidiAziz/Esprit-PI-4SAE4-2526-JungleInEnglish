9edc190434377e40cc8f6102e0a304fc903507da
SaidiAziz <155757450+SaidiAziz@users.noreply.github.com>
2026-05-07 01:49:05 +0100

    fix: update apiUrl for production and development environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8081',
+  apiUrl: 'http://localhost:30081',
   adminApiUrl: 'http://localhost:8090/admin/api/admin'
 };
 
